1e1990e13151f09cef1168ff42e5d89ff850af40
Tyler Lee <tylerlee@Tylers-MacBook-Air.local>
2022-06-12 17:47:45 -0700

    week1

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import profpic from "../assets/profpic.jpg";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+      <div class="split left">
+        <div className="centered">
+          <img
+            className="profile_image"
+            src={profpic}
+            alt="Profile Pic"
+            ></img>
+        </div>
       </div>
+      <div className="split right">
+        <div className="centered">
+          <div className="name_title">Tyler Lee</div>
+          <div className="brief_description">
+          Interests - swimming, movies, and gaming
+          </div>
+        </div>
+      </div>
+    </div>
     )
   }
 }
